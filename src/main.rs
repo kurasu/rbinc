686f5d16b3102691c5638a132acb90e91f4040c3
Claes Johanson <claes@bitwig.com>
2025-01-24 22:05:08 +0100

    Checked in dummy egui hello world app

---
+++ src/main.rs
@@ -0,0 +1,22 @@
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")] // hide console window on Windows in release
+#![allow(rustdoc::missing_crate_level_docs)] // it's an example
+
+use eframe::egui;
+
+fn main() -> eframe::Result {
+    env_logger::init(); // Log to stderr (if you run with `RUST_LOG=debug`).
+
+    let options = eframe::NativeOptions {
+        viewport: egui::ViewportBuilder::default().with_inner_size([600.0, 300.0]),
+        ..Default::default()
+    };
+
+    // Our application state:
+    let app_name = "bid-rs-demo";
+
+    eframe::run_simple_native(app_name, options, move |ctx, _frame| {
+        egui::CentralPanel::default().show(ctx, |ui| {
+            ui.heading(app_name);
+        });
+    })
+}
