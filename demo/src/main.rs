f544fa5f246cb52370a591b2d2927fd5e87f0096
Claes Johanson <claes@bitwig.com>
2025-01-31 10:56:12 +0100

    tree view

--- demo/src/main.rs
+++ demo/src/main.rs
@@ -4,9 +4,11 @@
 use std::fs::File;
 use std::io;
 use eframe::egui;
+use eframe::egui::Ui;
 use rfd::MessageLevel::Error;
 use binc::document::Document;
 use binc::repository::Repository;
+use uuid::Uuid;
 
 fn main() -> eframe::Result {
     env_logger::init(); // Log to stderr (if you run with `RUST_LOG=debug`).
@@ -39,13 +41,36 @@ fn main() -> eframe::Result {
                     save_document(&document);
                 }
             });
-            for node in document.nodes.keys() {
-                ui.label(format!("Node: {:?}", node));
-            }
+
+            create_tree(ui, &document);
         });
     })
 }
 
+fn create_tree(ui: &mut Ui, document: &Document) {
+    for node_id in document.nodes.iter() {
+        create_node_tree(ui, node_id.0, document);
+        return;
+    }
+}
+
+fn create_node_tree(ui: &mut Ui, node_id: &Uuid, document: &Document) {
+    if let Some(node) = document.nodes.get(node_id) {
+        let id_string = format!("ID: {:?}", node_id);
+        let name = node.attributes.get("name");
+        let label = if let Some(name) = name {
+            let name = name.downcast_ref::<String>().unwrap();
+            format!("{}", name)
+        }
+        else { id_string };
+
+        ui.collapsing(label, |ui| {
+            for child_id in &node.children {
+                create_node_tree(ui, child_id, document);
+            }
+        });
+    }
+}
 
 fn show_error<T>(result: io::Result<T>, description: &str) {
     if let Err(error) = result {
