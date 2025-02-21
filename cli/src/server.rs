8ec6d2e5abf31c0e34e370655a34db8aa64d7e80
Claes Johanson <claes@bitwig.com>
2025-02-21 09:11:10 +0100

    Don't limit incoming connections to localhost

--- cli/src/server.rs
+++ cli/src/server.rs
@@ -1,8 +1,8 @@
+use crate::store::Store;
+use binc::network_protocol::{NetworkRequest, NetworkResponse};
 use std::io;
 use std::io::{Read, Write};
 use std::net::{TcpListener, TcpStream};
-use binc::network_protocol::{NetworkRequest, NetworkResponse};
-use crate::store::Store;
 
 struct Connection {
     stream: TcpStream,
@@ -10,7 +10,7 @@ struct Connection {
 }
 
 pub(crate) fn server(store: String, port: u16) {
-    let addr = format!("localhost:{}", port);
+    let addr = format!("0.0.0.0:{}", port);
     let listener = TcpListener::bind(addr).unwrap();
 
     for stream in listener.incoming() {
@@ -21,47 +21,76 @@ pub(crate) fn server(store: String, port: u16) {
 
         let mut connection = Connection::new(s, store.clone());
         let r = connection.handle_connection();
-        if let Err(r) = r { println!("Error: {}", r) }
+        if let Err(r) = r {
+            println!("Error: {}", r)
+        }
     }
 }
 
 impl Connection {
-
     fn new(stream: TcpStream, root_dir: String) -> Connection {
-        Connection { stream, store: Store::new(&root_dir) }
+        Connection {
+            stream,
+            store: Store::new(&root_dir),
+        }
     }
 
-    pub fn handle_connection(&mut self) -> io::Result<()>{
+    pub fn handle_connection(&mut self) -> io::Result<()> {
         loop {
             let mut stream = &self.stream;
             let peer = stream.peer_addr()?;
             let request = NetworkRequest::read(&mut stream);
 
-            if let Ok(request) = request
-            {
+            if let Ok(request) = request {
                 println!("{peer} request: {}", request);
 
                 match request {
                     NetworkRequest::Disconnect => {
                         return Ok(());
-                    },
-                    NetworkRequest::ListFiles{ path } => {
-                        NetworkResponse::ListFiles { files: self.store.list_files(path)? }.write(&mut stream)?;
-                    },
-                    NetworkRequest::CreateFile { path } => {
-                        NetworkResponse::CreateFile { result: self.store.create_file(path).map_err(|e| e.to_string()) }.write(&mut stream)?;
-                    },
-                    NetworkRequest::GetFileData { from_revision, path } => {
-                        if let Ok((from_revision, to_revision , data)) = self.store.get_file_data(from_revision, path) {
-                            NetworkResponse::GetFileData { from_revision, to_revision , data}.write(&mut stream)?;
+                    }
+                    NetworkRequest::ListFiles { path } => {
+                        NetworkResponse::ListFiles {
+                            files: self.store.list_files(path)?,
                         }
-                    },
-                    NetworkRequest::AppendFile { from_revision, to_revision, path, data} => {
-                        NetworkResponse::AppendFile { result: self.store.append_file(from_revision, to_revision, &path, data).map_err(|e| e.to_string()) }.write(&mut stream)?;
+                        .write(&mut stream)?;
+                    }
+                    NetworkRequest::CreateFile { path } => {
+                        NetworkResponse::CreateFile {
+                            result: self.store.create_file(path).map_err(|e| e.to_string()),
+                        }
+                        .write(&mut stream)?;
+                    }
+                    NetworkRequest::GetFileData {
+                        from_revision,
+                        path,
+                    } => {
+                        if let Ok((from_revision, to_revision, data)) =
+                            self.store.get_file_data(from_revision, path)
+                        {
+                            NetworkResponse::GetFileData {
+                                from_revision,
+                                to_revision,
+                                data,
+                            }
+                            .write(&mut stream)?;
+                        }
+                    }
+                    NetworkRequest::AppendFile {
+                        from_revision,
+                        to_revision,
+                        path,
+                        data,
+                    } => {
+                        NetworkResponse::AppendFile {
+                            result: self
+                                .store
+                                .append_file(from_revision, to_revision, &path, data)
+                                .map_err(|e| e.to_string()),
+                        }
+                        .write(&mut stream)?;
                     }
                 }
-            }
-            else if let Err(request) = request {
+            } else if let Err(request) = request {
                 return Err(request);
             }
         }
